142a0c9e514c55d8c6d5a639f9dbe11c087e6ef0
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-04-09 15:31:15 -0400

    updated file

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+alert("connected");
+
+//psuedo coding
+//create a trivia game with ten question.
+//player is given 28 seconds to answer all questions.
+//checked answer right or wrong
+//initialize question variables
+var questionArray = [
+    "What does CSS means?",
+    "The web is based on",
+    "What does HTML stands for?",
+    "What symbol indicates a tag?",
+    "Which of these is a genuine tag?",
+    "What is the newest version of HTML?"
+];
+console.log(questionArray);
+
+var answerArray=[
+    ["Cascading Style Sheet","Cool Stylus Sheet","Column Style Sheet"],
+    ["Images", "Text", "Information", "HTML"],
+    ["Hyper Tag Markup language","Hyper Text Markup Language","Hyperlinks Text Mark laguage","Hyper Text Marking Language"],
+    ["Angle bracket e.g.","Curved brakets e.g.{,}","Commas e.g.','", "Exclamation marks e.g.!"]
+    ["header", "bold", "body", "image"],
+    ["HTML4", "HTML3", "HTML5","There is only one HTML"]
+];
+
+var rigthAnswers = [0, 3, 1, 0, 0, 2];
+
+
+//console.log("rigthAnswers[0]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
